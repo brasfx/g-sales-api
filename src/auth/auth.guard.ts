56d903726fc1f77bef2718ab0188f58121af4dcd
Davi Ribeiro <davi.ribeiro@gria.io>
2025-11-03 20:09:08 -0300

    Insert guard using jwt

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,11 +1,5 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Injectable } from '@nestjs/common';
+import { AuthGuard as Guard } from '@nestjs/passport';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
-  }
-}
+export class AuthGuard extends Guard('jwt') {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,18 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 
-@Module({})
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'chave-secreta',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
+})
 export class AuthModule {}
